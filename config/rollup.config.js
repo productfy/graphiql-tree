1c3e28678d2828c1fc9721b6d94aa33c5cb39bd4
Bao Thien Ngo <bingo@productfy.io>
2020-11-19 14:34:20 -0800

    Updating build

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -3,7 +3,6 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import pkg from '../package.json';
 import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
@@ -66,12 +65,12 @@ export default [
     external: ['react'],
     output: [
       {
-        file: pkg.main,
+        file: 'dist/index.js',
         format: 'cjs',
         exports: 'named',
       },
       {
-        file: pkg.module,
+        file: 'dist/index.es.js',
         format: 'esm',
         exports: 'named',
       },
