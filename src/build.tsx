a98ab5ef8b7031455e3fa2c0ec0233dbefe20f31
Bao Thien Ngo <bingo@productfy.io>
2021-03-07 19:45:13 -0800

    feat(opensource): Updating repo for open-sourcing
    1. Updated graphiql to 1.4.0
    2. Added public/index file
    3. Added LICENSE
    4. Updated build to export a few more types

--- src/build.tsx
+++ src/build.tsx
@@ -1,5 +1,7 @@
 export { default } from './GraphiQLTree';
+export type { default as DefaultValueCustomizer } from './DefaultValueCustomizer';
 export type { GraphiQLWithTreeProps } from './GraphiQLWithTree';
+export type { default as NodeCustomizer, NodeCustomizerParams } from './NodeCustomizer';
 export type { default as ParentDefinition } from './ParentDefinition';
 export type { default as Snippet } from './snippets/Snippet';
 export * as graphqlHelper from './graphqlHelper';
